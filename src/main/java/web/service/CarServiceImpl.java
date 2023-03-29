cb309d02ef1367dffa44543cf0cfdfde7bdc219a
Javi <katajavikendo@gmail.com>
2023-03-29 16:21:32 +0500

    Refactoring. Extra lines removed.

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -3,7 +3,6 @@ package web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.dao.CarDao;
-import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.List;
@@ -15,11 +14,6 @@ public class CarServiceImpl implements CarService {
     @Autowired
     public CarServiceImpl(CarDao carDao) {
         this.carDao = carDao;
-    }
-
-    @Override
-    public List<Car> getAllCars() {
-        return carDao.getAllCars();
     }
 
     @Override
